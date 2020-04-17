d68520e6052dd01b701acdb1cbfb34e7f62cedbc
Alexey Zadorozhny <alexzad@Alexeys-MacBook-Pro.local>
2020-04-17 17:20:32 -0500

    first deploy

---
+++ precache-manifest.5c1f779c7e639a7b3c75bd6120ec5426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68e24d7f3ef0df5c7f79b6e4678ab9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c394f9e2adce5a26d643",
+    "url": "/static/css/main.6bd3c04e.chunk.css"
+  },
+  {
+    "revision": "bce2f7832ff694ad0d2d",
+    "url": "/static/js/2.aa227f94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aa227f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c394f9e2adce5a26d643",
+    "url": "/static/js/main.075f42a5.chunk.js"
+  },
+  {
+    "revision": "0c6efc30b7e070c9b083",
+    "url": "/static/js/runtime-main.a793c2e5.js"
+  }
+]);
